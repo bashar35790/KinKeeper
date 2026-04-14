261f57c204ed782f58461a53707c9a8d2ed16a17
Abul Bashar <bashar35790@gmail.com>
2026-04-14 23:54:31 +0600

    added loading animatin on the Home page

--- src/pages/dashbord/Dashbord.jsx
+++ src/pages/dashbord/Dashbord.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Hero from "../../components/sections/hero/Hero";
 import StatisticCard from "../../components/utils/statisticCard/StatisticCard";
 import FriendCard from "../../components/utils/friendCard/FriendCard";
@@ -9,15 +9,20 @@ export default function Dashbord() {
   return (
     <>
       <Hero></Hero>
+
       <section>
         <StatisticCard />
         <div className="max-w-277.5 mx-auto px-4 pt-10 pb-20 space-y-4">
           <h2 className="text-heading text-2xl font-semibold">Your Friends</h2>
-          <div className="w-full grid grid-cols-4 gap-4">
-            {friendsData.map((friend) => (
-              <FriendCard key={friend.id} friend={friend} />
-            ))}
-          </div>
+          <Suspense
+            fallback={<span className="loading loading-bars loading-md"></span>}
+          >
+            <div className="w-full grid grid-cols-4 gap-4">
+              {friendsData.map((friend) => (
+                <FriendCard key={friend.id} friend={friend} />
+              ))}
+            </div>
+          </Suspense>
         </div>
       </section>
     </>
