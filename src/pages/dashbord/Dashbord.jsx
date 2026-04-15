a6b1c2218a23f5dbf2e809e74595d74536a65ee0
Abul Bashar <bashar35790@gmail.com>
2026-04-15 11:08:42 +0600

    fixed the home page responsiveness

--- src/pages/dashbord/Dashbord.jsx
+++ src/pages/dashbord/Dashbord.jsx
@@ -18,7 +18,7 @@ export default function Dashbord() {
         <div className="max-w-277.5 mx-auto px-4 pt-10 pb-20 space-y-4">
           <h2 className="text-heading text-2xl font-semibold">Your Friends</h2>
 
-          <div className="w-full grid grid-cols-4 gap-4">
+          <div className="w-full grid md:grid-cols-2 lg:grid-cols-4 gap-4">
             {friendsData.map((friend) => (
               <FriendCard key={friend.id} friend={friend} />
             ))}
